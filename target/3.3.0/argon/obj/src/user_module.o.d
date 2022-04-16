29366d983525996fb4e8829e6970727ed3b70e07
Mark Blashki <markblashki1@gmail.com>
2022-04-16 14:35:55 +1000

    chore: update git ignore list

--- target/3.3.0/argon/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-g\:/Mark/Documents/DOCUMENTS/Deakin/SIT210_Embedded_Systems_Development/3.2C/3-2C/target/3.3.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
